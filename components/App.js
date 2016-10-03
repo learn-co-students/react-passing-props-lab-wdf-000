c97fa784d56ca7bad54187e5182ee797cc349a93
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-10-03 12:34:10 -0400

    Done

--- components/App.js
+++ components/App.js
@@ -2,10 +2,42 @@ const React = require('react');
 
 const FruitBasket = require('./FruitBasket');
 
-const App = () => {
-  return (
-    <FruitBasket />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fruit: [],
+      filters: [],
+      selectedFilter: null,
+      items: []
+    };
+ 
+    
+    this.fetchFilters = this.fetchFilters.bind(this);
+  }
+
+
+  componentWillMount() {
+    this.fetchFilters();
+     fetch('/api/fruit')
+       .then(res => res.json())
+       .then(fruit => this.setState({ items: fruit}));
+         
+  }
+
+  fetchFilters() {
+    fetch('/api/fruit_types')
+      .then(res => res.json())
+      .then(filters => this.setState({filters: filters}));
+  }
+
+  render() {
+    return (
+      <FruitBasket selectedFilter={this.state.selectedFilter} filters={this.state.filters} items={this.state.items}/>
+    );
+  }
+
 }
 
 module.exports = App;
