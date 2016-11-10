c469580397a38912b555ecf2cc4b381a15b0808b
satub <satu.barnhill@gmail.com>
2016-11-10 13:59:04 -0500

    Done with a bugfix

--- components/FilteredFruitList.js
+++ components/FilteredFruitList.js
@@ -1,32 +1,18 @@
 const React = require('react');
-const { Component } = require('react');
 
-class FilteredFruitList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: []
-    };
-  }
-
-  componentWillMount() {
-    fetch('/api/fruit')
-      .then(res => res.json())
-      .then(fruit => this.setState({ items: fruit}));
-  }
-
-  render() {
-    const list = !this.props.filter ? this.state.items :
-      this.state.items.filter(i => i.fruit_type == this.props.filter);
+function FilteredFruitList(props){
+  let fruitList = (props.filter === 'all' || !props.filter) ? props.fruit :
+    props.fruit.filter(fructus => fructus.fruit_type == props.filter);
 
     return (
       <ul className="fruit-list">
-        {list.map((i,idx) => <li key={idx}>{i.char}</li>)}
+        {fruitList.map((i,idx) => <li key={idx}>{i.char}</li>)}
       </ul>
     );
-  }
+}
+
+FilteredFruitList.defaultProps = {
+  fruit: [], filter: 'all'
 }
 
 module.exports = FilteredFruitList;
-
